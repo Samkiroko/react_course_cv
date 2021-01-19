5b030d3d9fb6945fa94b37eb7b3ae419f494c6ef
Samkiroko <kirokodev@gmail.com>
2021-01-19 16:20:26 +0300

    Css componet styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Inlinestyling from './components/Inlinestyling';
 // import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 // import { Greet } from './components/Greet';
@@ -11,11 +12,16 @@ import Stylesheet from './components/Stylesheet';
 // import EventBind from './components/EventBind';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
+import './myApp.css';
+import styles from './myApp.module.css';
 
 function App() {
   return (
     <div className="App">
       <Stylesheet primary={true} />
+      <Inlinestyling />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>success</h1>
       {/* <NameList /> */}
       {/* <ParentComponent /> */}
       {/* <Greet name="Samuel" heroName="Superman">
---
+++ src/components/Inlinestyling.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const heading = {
+  fontSize: '75px',
+  color: 'orange',
+  fontWeight: 800,
+};
+
+function Inlinestyling() {
+  return (
+    <div>
+      <h1 style={heading}>Inline styling</h1>
+    </div>
+  );
+}
+
+export default Inlinestyling;
