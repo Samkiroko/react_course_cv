ccff50b9d3246dccaa8cc8a1cefafcf9834497cf
Samkiroko <kirokodev@gmail.com>
2021-01-22 11:02:58 +0300

    Component mounting lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form1 from './components/Form1';
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form1 />
+        <LifecycleA />
       </div>
     );
   }
